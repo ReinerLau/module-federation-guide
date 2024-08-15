c375e7f1da2e8009c7abcbdc59e072a16cbf93f8
ReinerLau <lk850593913@gmail.com>
2024-08-15 17:00:03 +0800

    init

--- remote/vite.config.ts
+++ remote/vite.config.ts
@@ -2,29 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx(),
     vueDevTools(),
-    federation({
-      name: 'remote',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './HelloWorld': './src/components/HelloWorld.vue'
-      },
-      shared: ['vue']
-    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    target: 'esnext'
   }
 })
